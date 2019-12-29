b18c1f36aeec22cc64c2c2200eac51d42cc0349c
Vaibhavnayan <vaibhav.16.nayan@gmail.com>
2019-12-29 10:24:50 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import turtle
+a =5
+print(a)
+b="Hello"
+print(b)
+
+vaibhav_turtle= turtle.Turtle()
+
+def square():
+  vaibhav_turtle.forward(100)
+  vaibhav_turtle.right(90)
+  vaibhav_turtle.forward(100)
+  vaibhav_turtle.right(90)
+  vaibhav_turtle.forward(100)
+  vaibhav_turtle.right(90)
+  vaibhav_turtle.forward(100)
+
+square()
+square()
